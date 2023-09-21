facca90184064fad43db68521f4c37f64c490064
mohit <mohitsingh732@yahoo.com>
2023-09-22 00:18:24 +0530

    Minor UI tweeks

--- app/src/main/java/com/ncs/tradezy/ActionBar.kt
+++ app/src/main/java/com/ncs/tradezy/ActionBar.kt
@@ -2,6 +2,25 @@ package com.ncs.tradezy
 
 import android.content.Intent
 import android.util.Log
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOut
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.with
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -14,17 +33,26 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,29 +61,40 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.times
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.google.firebase.auth.FirebaseAuth
+import com.ncs.tradezy.repository.RealTimeUserResponse
 import com.ncs.tradezy.ui.theme.accent
 import com.ncs.tradezy.ui.theme.betterWhite
 import com.ncs.tradezy.ui.theme.primary
 import com.ncs.tradezy.ui.theme.secondary
-
-
+import kotlinx.coroutines.delay
 
 
 @Composable
-fun setActionBar(screenName:String, image: Int,navController: NavController,viewModel: NotificationViewModel= hiltViewModel(),viewModel2: ChatViewModel= hiltViewModel()) {
+fun setActionBar(screenName:String, image: Int,navController: NavController,viewModel: NotificationViewModel= hiltViewModel(),viewModel2: ChatViewModel= hiltViewModel(),userViewModel:ProfileActivityViewModel= hiltViewModel()) {
     val res = viewModel.res.value
+    val user=userViewModel.res.value
     var noticount = 0
     var messagecount=0
     val currentuser = FirebaseAuth.getInstance().currentUser?.uid
     var filtereNotiList = ArrayList<NotificationContent>()
+    var current = ArrayList<RealTimeUserResponse>()
     for (i in 0 until res.item.size) {
         if (res.item[i].item?.receiverID == currentuser) {
             filtereNotiList.add(res.item[i])
+        }
+    }
+    if (user.item.isNotEmpty()) {
+        for (i in 0 until user.item.size) {
+            if (user.item[i].item?.userId == currentuser) {
+                current.add(user.item[i])
+            }
         }
     }
     for (i in 0 until filtereNotiList.size) {
@@ -100,7 +139,7 @@ fun setActionBar(screenName:String, image: Int,navController: NavController,view
             }
         }
     }
-
+    
 
 
     Column {
@@ -117,92 +156,41 @@ fun setActionBar(screenName:String, image: Int,navController: NavController,view
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Box(Modifier.padding(15.dp)) {
-                        Text(
-                            text = screenName,
-                            color = accent,
-                            fontSize = 25.sp,
-                            fontWeight = FontWeight.Medium
-                        )
 
+
+                            Text(
+                                text =if (user.item.isEmpty()) "Welcome" else "Hi, ${current[0].item?.name?.substringBefore(" ")}" ,
+                                color = accent,
+                                fontSize = 25.sp,
+                                fontWeight = FontWeight.Medium
+                            )
                     }
                     Row {
-                        Box(Modifier.fillMaxHeight(), contentAlignment = Alignment.Center) {
-                            Row {
-                                Box(Modifier.fillMaxHeight()){
-                                    Box(Modifier.padding(top = 5.dp)) {
-                                        Icon(imageVector = Icons.Filled.Notifications,
-                                            contentDescription = "",
-                                            tint = betterWhite,
-                                            modifier = Modifier
-                                                .clickable {
-                                                    navController.navigate("notificationScreen")
-                                                }
-                                                .size(30.dp))
-                                    }
-                                    Box(Modifier.padding(start = 15.dp, bottom = 10.dp)){
-                                        Text(
-                                            text = noticount.toString(),
-                                            Modifier
-                                                .clip(CircleShape)
-                                                .size(20.dp)
-                                                .background(
-                                                    Color.Red
-                                                ),
-                                            fontSize = 15.sp,
-                                            color = betterWhite,
-                                            textAlign = TextAlign.Center
-                                        )
-                                    }
-
-                                }
-
-                                Spacer(modifier = Modifier.width(10.dp))
-                                Text(
-                                    text = messagecount.toString(),
-                                    Modifier
-                                        .clip(CircleShape)
-                                        .size(25.dp)
-                                        .background(
-                                            Color.Red
-                                        ),
-                                    fontSize = 20.sp,
-                                    color = betterWhite,
-                                    textAlign = TextAlign.Center
-                                )
-                                Icon(imageVector = Icons.Filled.Email,
+                        Box(Modifier.fillMaxHeight().padding(end = 25.dp, top = 10.dp).clickable { context.startActivity(Intent(context,ChatActivity::class.java)) }){
+                            Box(Modifier.padding(top = 5.dp)) {
+                                Icon(painter = painterResource(R.drawable.msg_icon),
                                     contentDescription = "",
-                                    tint = betterWhite,
                                     modifier = Modifier
-                                        .clickable {
-                                            context.startActivity(
-                                                Intent(
-                                                    context,
-                                                    ChatActivity::class.java
-                                                )
-                                            )
-
-                                        }
-                                        .size(25.dp))
-
+                                        .size(30.dp))
                             }
-
-                        }
-                        Box(
-                            modifier = Modifier
-                                .clip(RoundedCornerShape(25.dp))
-                        ) {
-                            Image(
-                                painterResource(id = image),
-                                contentDescription = "",
-                                Modifier.clickable {
-                                    context.startActivity(
-                                        Intent(
-                                            context,
-                                            ProfileActivity::class.java
-                                        )
+                            if (messagecount>0){
+                                Box(Modifier.padding(start = 21.dp, bottom = 15.dp)){
+                                    Text(
+                                        text = messagecount.toString(),
+                                        Modifier
+                                            .clip(CircleShape)
+                                            .size(18.dp)
+                                            .background(
+                                                Color.Red
+                                            ),
+                                        fontSize = 12.sp,
+                                        color = betterWhite,
+                                        textAlign = TextAlign.Center
                                     )
-                                })
+                                }
+                            }
                         }
+
                     }
 
                 }
